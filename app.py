e0a5b1a3589aa370ecf4421ba101c0c0315a60d2
Aniket <aniketgajare999@.com>
2023-06-24 11:31:59 +0530

    my web app is ready

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+import json
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+#load model
+ramodel=pickle.load(open('bankmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+
+
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=ramodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = ramodel.predict(final_input)[0]
+    return render_template('home.html', prediction_text='The company bankruptcy prediction is {}'.format(output))
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
